77b54c932fb632a2260f3ad1126dd2a7d44fae26
kb <lkb0398@nate.com>
2025-10-24 21:30:42 +0900

    Update detail_item.dart

---
+++ lib/view/detail_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailItem extends StatelessWidget {
+  const DetailItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
