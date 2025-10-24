9f11834912f59e95b881d24d81fd77144c88c3a3
kb <lkb0398@nate.com>
2025-10-25 00:07:55 +0900

    Update home_page.dart

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_market_sajo/model/item_model.dart';
-import 'package:flutter_market_sajo/view/Item_list.dart';
+import 'package:flutter_market_sajo/view/item_list.dart';
 import 'package:flutter_market_sajo/view/add_item.dart';
 import 'package:flutter_market_sajo/view/no_item_list.dart';
 
