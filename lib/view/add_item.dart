7f7dc52ce633c731a537c356cc7af4602524ede6
kb <lkb0398@nate.com>
2025-10-24 21:30:40 +0900

    Update add_item.dart

--- lib/view/add_item.dart
+++ lib/view/add_item.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_market_sajo/model/item_model.dart';
 
 class AddItem extends StatelessWidget {
-  const AddItem({super.key});
+  const AddItem({super.key, required this.list});
+  final List<ItemModel> list;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      
+    );
   }
 }
