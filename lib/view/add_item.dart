38daf59c93155cbfedeca438ba0a741b849db717
kb <lkb0398@nate.com>
2025-10-28 21:24:44 +0900

    Update add_item.dart

--- lib/view/add_item.dart
+++ lib/view/add_item.dart
@@ -113,7 +113,7 @@ class _AddItemState extends State<AddItem> {
           ),
         ),
       ),
-      // 수정하기 버튼
+      // 등록하기 버튼
       bottomNavigationBar: GestureDetector(
         onTap: () {
           // 예외 처리
