e1b114a04ba6f37142e6c7b47043587da984260b
kb <lkb0398@nate.com>
2025-10-28 09:39:54 +0900

    Update item_model.dart

--- lib/model/item_model.dart
+++ lib/model/item_model.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ItemModel {
-  String? title; // 제목
   String productName; // 상품이름
   int price; // 상품 가격
   Image image; // 상품이미지
@@ -10,7 +9,6 @@ class ItemModel {
   bool cart; // 제품 개수
 
   ItemModel({
-    this.title,
     required this.productName,
     required this.price,
     required this.image,
