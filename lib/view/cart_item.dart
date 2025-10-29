9c559cf564e9c0522b700bf1d569a1bb77126354
kb <lkb0398@nate.com>
2025-10-29 17:14:42 +0900

    Update cart_item.dart

--- lib/view/cart_item.dart
+++ lib/view/cart_item.dart
@@ -19,114 +19,111 @@ class _CartItemState extends State<CartItem> {
     return ListView.builder(
       itemCount: widget.list.length,
       itemBuilder: (context, index) {
-        return Container(
-          margin: EdgeInsets.all(10),
-          child: InkWell(
-            onLongPress: () {
-              showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                  title: Text("장바구니에서 삭제하시겠습니까?"),
-                  actions: [
-                    TextButton(
-                      onPressed: () {
-                        setState(() {
-                          widget.list[index].productCount = 1;
-                          widget.list[index].cart = false;
-                        });
+        return InkWell(
+          onLongPress: () {
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: Text("장바구니에서 삭제하시겠습니까?"),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
                         Navigator.of(context).pop();
-                        Navigator.of(context).pushReplacement(
-                          MaterialPageRoute(
-                            builder: (context) => Cart(list: widget.list),
-                          ),
-                        );
-                      },
-                      child: Text("삭제"),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        setState(() {
-                          Navigator.of(context).pop();
-                        });
-                      },
-                      child: Text("취소"),
-                    ),
-                  ],
-                ),
-              );
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border(bottom: BorderSide(color: Colors.white)),
+                      });
+                    },
+                    child: Text("취소"),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        widget.list[index].productCount = 1;
+                        widget.list[index].cart = false;
+                      });
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => Cart(list: widget.list),
+                        ),
+                      );
+                    },
+                    child: Text("삭제"),
+                  ),
+                ],
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(12),
-                      child: widget.list[index].image,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      spacing: 10,
-                      children: [
-                        AutoSizeText(
-                          maxLines: 1,
-                          widget.list[index].productName,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
+            );
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border(bottom: BorderSide(color: Colors.white)),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: widget.list[index].image,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    spacing: 10,
+                    children: [
+                      AutoSizeText(
+                        maxLines: 1,
+                        widget.list[index].productName,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
                         ),
-                        AutoSizeText(
-                          maxLines: 1,
-                          "${NumberFormat("#,###").format(widget.list[index].price * widget.list[index].productCount)}원",
-                          style: TextStyle(fontSize: 25, color: Colors.red),
+                      ),
+                      AutoSizeText(
+                        maxLines: 1,
+                        "${NumberFormat("#,###").format(widget.list[index].price * widget.list[index].productCount)}원",
+                        style: TextStyle(fontSize: 25, color: Colors.red),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white24),
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                        Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.white24),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Row(
-                            spacing: 5,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              InkWell(
-                                onTap: () {
-                                  setState(() {
-                                    widget.list[index].productCount--;
-                                    if (widget.list[index].productCount < 1) {
-                                      widget.list[index].productCount = 99;
-                                    }
-                                  });
-                                },
-                                child: Icon(Icons.remove, color: Colors.red),
-                              ),
-                              Text(
-                                "${widget.list[index].productCount}개",
-                                style: TextStyle(fontSize: 20),
-                              ),
-                              InkWell(
-                                onTap: () {
-                                  setState(() {
-                                    widget.list[index].productCount++;
-                                    if (widget.list[index].productCount > 99) {
-                                      widget.list[index].productCount = 1;
-                                    }
-                                  });
-                                },
-                                child: Icon(Icons.add, color: Colors.red),
-                              ),
-                            ],
-                          ),
+                        child: Row(
+                          spacing: 5,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  widget.list[index].productCount--;
+                                  if (widget.list[index].productCount < 1) {
+                                    widget.list[index].productCount = 99;
+                                  }
+                                });
+                              },
+                              child: Icon(Icons.remove, color: Colors.red),
+                            ),
+                            Text(
+                              "${widget.list[index].productCount}개",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  widget.list[index].productCount++;
+                                  if (widget.list[index].productCount > 99) {
+                                    widget.list[index].productCount = 1;
+                                  }
+                                });
+                              },
+                              child: Icon(Icons.add, color: Colors.red),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ],
-                ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ),
